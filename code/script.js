a6c838df25862c530ca553a6416475652afa5fb6
ahrirubin <ahri.rubin@gmail.com>
2023-11-14 10:21:15 +0100

    first commit

---
+++ code/script.js
@@ -0,0 +1,35 @@
+
+
+// A function that adds and remove the class "active" on the section you click on.
+function toggle(e) {
+  const element = e.target;
+  element.classList.toggle("active");
+}
+
+// Selects and HTML element, and calls a function which will be executed when the element is clicked.
+const section1Element = document.getElementById("section1");
+section1Element.addEventListener("click", toggle);
+
+const section2Element = document.getElementById("section2");
+section2Element.addEventListener("click", toggle);
+
+const section3Element = document.getElementById("section3");
+section3Element.addEventListener("click", toggle);
+
+
+//
+async function getPosts() {
+  const response = await fetch("https://jsonplaceholder.typicode.com/posts");
+  const list = await response.json();
+  list.forEach((item) => {
+    post = item;
+    body = item.body
+    title = item.title
+    console.log(post, body, title)
+  });
+  console.log(list);
+  return list;
+}
+getPosts()
+
+
