b3e620cb1795e9f0bd63812b5611a7d77ac7b485
ahrirubin <ahri.rubin@gmail.com>
2023-11-18 00:14:01 +0100

    first commit

--- code/script.js
+++ code/script.js
@@ -4,32 +4,36 @@
 function toggle(e) {
   const element = e.target;
   element.classList.toggle("active");
+  console.log(e,"div is active")
 }
 
 // Selects and HTML element, and calls a function which will be executed when the element is clicked.
-const section1Element = document.getElementById("section1");
-section1Element.addEventListener("click", toggle);
-
-const section2Element = document.getElementById("section2");
-section2Element.addEventListener("click", toggle);
-
-const section3Element = document.getElementById("section3");
-section3Element.addEventListener("click", toggle);
-
-
-//
 async function getPosts() {
   const response = await fetch("https://jsonplaceholder.typicode.com/posts");
   const list = await response.json();
-  list.forEach((item) => {
-    post = item;
-    body = item.body
-    title = item.title
-    console.log(post, body, title)
-  });
-  console.log(list);
-  return list;
+  const faqContainer = document.getElementById("faq");
+
+list.forEach((item) => {
+  
+  const titleDiv = document.createElement("div");
+  titleDiv.addEventListener("click", (event) => {
+    event.currentTarget.classList.toggle("active")
+    console.log("div is active", event.target)  });
+  titleDiv.setAttribute("class", "title");
+  titleDiv.innerHTML = `<p class="titleText">${item.title + "?"}</p>`;
+
+  const descDiv = document.createElement("div");
+  descDiv.setAttribute("class", "description");
+  descDiv.innerHTML = item.body;
+
+  faqContainer.appendChild(titleDiv);
+  titleDiv.appendChild(descDiv)
+
+})
+
+  
 }
 getPosts()
+
 
 
